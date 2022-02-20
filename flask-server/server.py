bb9157eb41cb22fce0cea6eb9769b6d3b5e2efb6
Crystal Word <cword1991@gmail.com>
2022-02-20 12:11:10 -0500

    cleaning up files and re-adding venv

---
+++ flask-server/server.py
@@ -0,0 +1,14 @@
+#This will be the server
+from distutils.log import debug
+from flask import Flask, request, send_from_directory
+
+app = Flask(__name__,static_url_path='/static/public/index.html')
+
+@app.route("/")
+def send_js(path):
+    return send_from_directory('js',path)
+
+if __name__ == '__main__': 
+    app.run('0.0.0.0', 5000, debug=True)
+    
+
