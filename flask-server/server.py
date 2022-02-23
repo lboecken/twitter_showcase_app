6c713ce4a38d58ec53d978e252ceadb47c03f47c
Crystal Word <cword1991@gmail.com>
2022-02-23 01:43:20 -0500

    testing external API functionality

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,10 @@
-#This will be the server
+from crypt import methods
 from distutils.log import debug
-from flask import Flask, request, send_from_directory
+import json
+from flask import Flask, jsonify, send_from_directory
+import requests
+
+
 
 app = Flask(__name__, static_folder="../build/")
 
@@ -8,6 +12,27 @@ app = Flask(__name__, static_folder="../build/")
 @app.route('/<path:path>')
 def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route("/serve")
+def search():
+    
+    try:
+        r = requests.get("http://swapi.dev/api/people/1233223/")
+        data = r.json()
+        name = data["name"]
+        eye_color = data["eye_color"]
+        hair_color = data["hair_color"]
+        return jsonify({"name": name, "eye_color": eye_color, "hair_color": hair_color})
+    except:
+        
+        return "Something Went Wrong..."
+
+    
+
+
+
+
 
 
 if __name__ == '__main__': 
